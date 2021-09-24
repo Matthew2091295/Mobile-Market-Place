cc4184026a3e766ed9053f1e715e9fa673935262
Anri Pienaar <66550000+orchid98@users.noreply.github.com>
2021-09-25 00:42:03 +0200

    Create Cart_test.dart

---
+++ test/Cart_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import 'package:market_place/Cart.dart';
+import 'package:market_place/widgets/CartItem.dart';
+
+void main() {
+  String fromRichTextToPlainText(final Widget widget) {
+    if (widget is RichText) {
+      if (widget.text is TextSpan) {
+        final buffer = StringBuffer();
+        (widget.text as TextSpan).computeToPlainText(buffer);
+        return buffer.toString();
+      }
+    }
+    return null;
+  }
+
+  //Product used to test cart item widget
+  String productIcon =
+      "https://media.takealot.com/covers_tsins/57593134/6009546804725-1-zoom.jpg";
+  String productName = "Cat's Life Cat Cube Floral (Pink06) M";
+  double productPrice = 590;
+
+  double iconHeight = 100;
+
+  testWidgets("Test Network Image", (WidgetTester tester) async {
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+            home: new CartItem(
+          productIcon: productIcon,
+          productName: productName,
+          productPrice: productPrice,
+          iconHeight: iconHeight,
+        )));
+
+    mockNetworkImagesFor(() => tester.pumpWidget(testWidget));
+  });
+
+  testWidgets("Test Rich Texts and Text Button", (WidgetTester tester) async {
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(), child: new MaterialApp(home: new Cart()));
+
+    await tester.pumpWidget(testWidget);
+
+    expect(
+        find.byWidgetPredicate(
+            (testWidget) => fromRichTextToPlainText(testWidget) == "Total:"),
+        findsOneWidget);
+
+    expect(find.widgetWithText(TextButton, "Checkout"), findsOneWidget);
+  });
+}
