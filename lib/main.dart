a050ccb3e9a5ae2e297f45b875314abcd519f99a
Anri Pienaar <66550000+orchid98@users.noreply.github.com>
2021-10-03 00:11:50 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 import 'package:market_place/Login.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(ProviderScope(
+      child: MaterialApp(
     home: Main(),
-  ));
+  )));
 }
 
 class Main extends StatefulWidget {
-  @override  
+  @override
   _MainState createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
