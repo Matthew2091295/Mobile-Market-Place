d4de33427c60a620cb6c13cae0123cd9b1323ab9
Anri Pienaar <66550000+orchid98@users.noreply.github.com>
2021-10-04 14:55:35 +0200

    Update Cart_test.dart

--- test/Cart_test.dart
+++ test/Cart_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:market_place/CartNotifier.dart';
+import 'package:market_place/Notifiers.dart';
 import 'package:market_place/Providers.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
