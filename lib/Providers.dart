bc49a8f15828d6ea8066324a1d272a34fe8e9a16
Anri Pienaar <66550000+orchid98@users.noreply.github.com>
2021-10-04 14:42:40 +0200

    Update Providers.dart

--- lib/Providers.dart
+++ lib/Providers.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:market_place/CartNotifier.dart';
+import 'package:market_place/Notifiers.dart';
 
 final cartProvider = ChangeNotifierProvider((ref) => CartNotifier());
