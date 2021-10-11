b3e1a0968a9eb8ca27b17cc2b8da09071fbfe12e
Anri Pienaar <66550000+orchid98@users.noreply.github.com>
2021-10-11 18:42:59 +0200

    Update Providers.dart

--- lib/Providers.dart
+++ lib/Providers.dart
@@ -4,4 +4,4 @@ import 'package:market_place/Notifiers.dart';
 final totalProvider = ChangeNotifierProvider((ref) => TotalNotifier());
 final walletProvider = ChangeNotifierProvider((ref) => WalletNotifier());
 final quantityProvider = ChangeNotifierProvider((ref) => QuantityNotifier());
-
+final countProvider = ChangeNotifierProvider((ref) => CountNotifier());
